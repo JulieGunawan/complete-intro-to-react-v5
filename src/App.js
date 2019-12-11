6b43cba4317722eddaefd4af791fc8fc7b555c22
Franklin Koch <franklin.koch@seequent.com>
2019-12-11 02:53:42 -0700

    Learn about JS tools

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-const Pet = ({name, animal, breed}) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ])
-}
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from "./Pet"
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { name: "gilda", animal: "dog", breed: "golden retriever"}),
-      React.createElement(Pet, { name: "stella", animal: "dog", breed: "dachshund"}),
-      React.createElement(Pet, { name: "taco", animal: "gecko", breed: "madagasdcar day gecko"})
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "gilda",
+      animal: "dog",
+      breed: "golden retriever"
+    }),
+    React.createElement(Pet, {
+      name: "stella",
+      animal: "dog",
+      breed: "dachshund"
+    }),
+    React.createElement(Pet, {
+      name: "taco",
+      animal: "gecko",
+      breed: "madagasdcar day gecko"
+    })
+  ]);
 };
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("root")
-);
+render(React.createElement(App), document.getElementById("root"));
